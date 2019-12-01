a743c2722a31c01fea6bcd86afcbec17310648ff
Kerber <DaIvan97@gmail.com>
2019-12-01 20:08:04 +0300

    Added case insensitive unordered map and set templates

--- KxFramework/KxComparator.h
+++ KxFramework/KxComparator.h
@@ -92,3 +92,49 @@ namespace KxComparator
 		return KxString::Compare(v1, v2, ignoreCase) == CompareResult::GreaterThan || KxString::Compare(v1, v2, ignoreCase) == CompareResult::Equal;
 	}
 }
+
+namespace KxComparator
+{
+	struct StringEqualToNoCase
+	{
+		bool operator()(const wxString& left, const wxString& right) const
+		{
+			return IsEqual(left, right, true);
+		}
+		bool operator()(std::wstring_view left, std::wstring_view right) const
+		{
+			return IsEqual(left, right, true);
+		}
+	};
+	struct StringHashNoCase
+	{
+		// From Boost
+		template<class T> static void hash_combine(size_t& seed, const T& v)
+		{
+			std::hash<T> hasher;
+			seed ^= hasher(v) + size_t(0x9e3779b9u) + (seed << 6) + (seed >> 2);
+		}
+
+		size_t operator()(const wxString& value) const
+		{
+			size_t hashValue = 0;
+			for (wxChar c: value)
+			{
+				hash_combine(hashValue, KxString::CharToLower(c));
+			}
+			return hashValue;
+		}
+		size_t operator()(std::wstring_view value) const
+		{
+			size_t hashValue = 0;
+			for (wchar_t c: value)
+			{
+				hash_combine(hashValue, KxString::CharToLower(c));
+			}
+			return hashValue;
+		}
+	};
+
+	template<class TKey, class TValue> using UMapNoCase = std::unordered_map<TKey, TValue, StringHashNoCase, StringEqualToNoCase>;
+	template<class TValue> using USetNoCase = std::unordered_set<TValue, StringHashNoCase, StringEqualToNoCase>;
+}
