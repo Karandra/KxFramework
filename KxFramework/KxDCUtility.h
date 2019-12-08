1e8870f1eda5c661d839fb7c23be763e648d7f81
Kerber <DaIvan97@gmail.com>
2019-12-09 01:16:36 +0300

    Added 'LogicalFunctionChanger' class

--- KxFramework/KxDCUtility.h
+++ KxFramework/KxDCUtility.h
@@ -6,3 +6,24 @@ namespace KxDCUtility
 {
 	KxColor GetAverageColor(const wxDC& dc, const wxRect& rect);
 }
+
+namespace KxDCUtility
+{
+	class LogicalFunctionChanger final
+	{
+		private:
+			wxDC& m_DC;
+			const wxRasterOperationMode m_OriginalMode = wxRasterOperationMode::wxCLEAR;
+
+		public:
+			LogicalFunctionChanger(wxDC& dc, wxRasterOperationMode newMode)
+				:m_DC(dc), m_OriginalMode(dc.GetLogicalFunction())
+			{
+				m_DC.SetLogicalFunction(newMode);
+			}
+			~LogicalFunctionChanger()
+			{
+				m_DC.SetLogicalFunction(m_OriginalMode);
+			}
+	};
+}
