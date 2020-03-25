a7f51b8806340a278dceea474561b1a16721c8f4
Kerber <DaIvan97@gmail.com>
2020-03-26 01:18:27 +0300

    KxEnumClassOperations used by default in Kx

--- Kx/Common.hpp
+++ Kx/Common.hpp
@@ -8,6 +8,16 @@
 #define KX_API
 #endif
 
+// Standard library
 #include <cstdint>
 #include <utility>
 #include <memory>
+
+// Some generic headers
+#include <KxFramework/KxEnumClassOperations.h>
+
+namespace KxFramework
+{
+	// Yes, this is the intended way of using 'KxEnumClassOperations' when you have your own namespace
+	using namespace KxEnumClassOperations;
+}
