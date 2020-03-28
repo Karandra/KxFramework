b4a694fdce5c0d0030543d252a8b527637a0163f
Kerber <DaIvan97@gmail.com>
2020-03-29 02:00:16 +0300

    Added template singleton classes

---
+++ Kx/General/Singleton.h
@@ -0,0 +1,73 @@
+#pragma once
+#include "Common.h"
+
+namespace KxFramework
+{
+	template<class T>
+	class Singleton
+	{
+		public:
+			using TObject = T;
+
+		public:
+			static T& GetInstance()
+			{
+				static T instance;
+				return instance;
+			}
+
+		private:
+			Singleton() = default;
+			~Singleton() = default;
+
+		public:
+			Singleton(const Singleton&) = delete;
+			Singleton(Singleton&&) = delete;
+
+		public:
+			Singleton& operator=(const Singleton&) = delete;
+			Singleton& operator=(Singleton&&) = delete;
+	};
+}
+
+namespace KxFramework
+{
+	template<class T>
+	class SingletonPtr
+	{
+		public:
+			using TObject = T;
+
+		private:
+			static inline T* ms_Instance = nullptr;
+
+		public:
+			static T* GetInstance()
+			{
+				return ms_Instance;
+			}
+
+		public:
+			SingletonPtr()
+			{
+				if (!ms_Instance)
+				{
+					ms_Instance = static_cast<T*>(this);
+				}
+				else
+				{
+					throw std::runtime_error("KxSingletonPtr: Only one instance of " __FUNCTION__ " is allowed");
+				}
+			}
+			SingletonPtr(const SingletonPtr&) = delete;
+			SingletonPtr(SingletonPtr&&) = delete;
+			virtual ~SingletonPtr()
+			{
+				ms_Instance = nullptr;
+			}
+		
+		public:
+			SingletonPtr& operator=(const SingletonPtr&) = delete;
+			SingletonPtr& operator=(SingletonPtr&&) = delete;
+	};
+}
