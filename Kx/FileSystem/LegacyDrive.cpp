0ffbcb2b7c7bc76ce46a37287cc25a2b914cd537
Kerber <DaIvan97@gmail.com>
2020-03-29 02:00:42 +0300

    Using 'ModFlagRef' from 'Utility' instead of local function

--- Kx/FileSystem/LegacyDrive.cpp
+++ Kx/FileSystem/LegacyDrive.cpp
@@ -1,6 +1,7 @@
 #include "KxStdAfx.h"
 #include "LegacyDrive.h"
 #include "FSPath.h"
+#include "Kx/Utility/Common.h"
 #include "KxFramework/KxFileStream.h"
 
 namespace KxFramework
@@ -45,34 +46,26 @@ namespace KxFramework
 	}
 	FileSystemFeature MapDiskFeatures(DWORD nativeFeatures)
 	{
-		auto ModFlag = [](auto& flag, auto flagMod, bool set)
-		{
-			if (set)
-			{
-				flag |= flagMod;
-			}
-		};
-
 		FileSystemFeature features = FileSystemFeature::None;
-		ModFlag(features, FileSystemFeature::CasePreservedNames, nativeFeatures & FILE_CASE_PRESERVED_NAMES);
-		ModFlag(features, FileSystemFeature::CaseSensitiveSearch, nativeFeatures & FILE_CASE_SENSITIVE_SEARCH);
-		ModFlag(features, FileSystemFeature::FileCompression, nativeFeatures & FILE_FILE_COMPRESSION);
-		ModFlag(features, FileSystemFeature::NamedStreams, nativeFeatures & FILE_NAMED_STREAMS);
-		ModFlag(features, FileSystemFeature::PersistentACLS, nativeFeatures & FILE_PERSISTENT_ACLS);
-		ModFlag(features, FileSystemFeature::ReadOnlyVolume, nativeFeatures & FILE_READ_ONLY_VOLUME);
-		ModFlag(features, FileSystemFeature::SequentialWrite, nativeFeatures & FILE_SEQUENTIAL_WRITE_ONCE);
-		ModFlag(features, FileSystemFeature::Encryption, nativeFeatures & FILE_SUPPORTS_ENCRYPTION);
-		ModFlag(features, FileSystemFeature::ExtendedAttributes, nativeFeatures & FILE_SUPPORTS_EXTENDED_ATTRIBUTES);
-		ModFlag(features, FileSystemFeature::HardLinks, nativeFeatures & FILE_SUPPORTS_HARD_LINKS);
-		ModFlag(features, FileSystemFeature::ObjectIDs, nativeFeatures & FILE_SUPPORTS_OBJECT_IDS);
-		ModFlag(features, FileSystemFeature::OpenFileByID, nativeFeatures & FILE_SUPPORTS_OPEN_BY_FILE_ID);
-		ModFlag(features, FileSystemFeature::ReparsePoints, nativeFeatures & FILE_SUPPORTS_REPARSE_POINTS);
-		ModFlag(features, FileSystemFeature::SparseFiles, nativeFeatures & FILE_SUPPORTS_SPARSE_FILES);
-		ModFlag(features, FileSystemFeature::Transactions, nativeFeatures & FILE_SUPPORTS_TRANSACTIONS);
-		ModFlag(features, FileSystemFeature::USNJournal, nativeFeatures & FILE_SUPPORTS_USN_JOURNAL);
-		ModFlag(features, FileSystemFeature::Unicode, nativeFeatures & FILE_UNICODE_ON_DISK);
-		ModFlag(features, FileSystemFeature::CompressedVolume, nativeFeatures & FILE_VOLUME_IS_COMPRESSED);
-		ModFlag(features, FileSystemFeature::VolumeQuotas, nativeFeatures & FILE_VOLUME_QUOTAS);
+		Utility::ModFlagRef(features, FileSystemFeature::CasePreservedNames, nativeFeatures & FILE_CASE_PRESERVED_NAMES);
+		Utility::ModFlagRef(features, FileSystemFeature::CaseSensitiveSearch, nativeFeatures & FILE_CASE_SENSITIVE_SEARCH);
+		Utility::ModFlagRef(features, FileSystemFeature::FileCompression, nativeFeatures & FILE_FILE_COMPRESSION);
+		Utility::ModFlagRef(features, FileSystemFeature::NamedStreams, nativeFeatures & FILE_NAMED_STREAMS);
+		Utility::ModFlagRef(features, FileSystemFeature::PersistentACLS, nativeFeatures & FILE_PERSISTENT_ACLS);
+		Utility::ModFlagRef(features, FileSystemFeature::ReadOnlyVolume, nativeFeatures & FILE_READ_ONLY_VOLUME);
+		Utility::ModFlagRef(features, FileSystemFeature::SequentialWrite, nativeFeatures & FILE_SEQUENTIAL_WRITE_ONCE);
+		Utility::ModFlagRef(features, FileSystemFeature::Encryption, nativeFeatures & FILE_SUPPORTS_ENCRYPTION);
+		Utility::ModFlagRef(features, FileSystemFeature::ExtendedAttributes, nativeFeatures & FILE_SUPPORTS_EXTENDED_ATTRIBUTES);
+		Utility::ModFlagRef(features, FileSystemFeature::HardLinks, nativeFeatures & FILE_SUPPORTS_HARD_LINKS);
+		Utility::ModFlagRef(features, FileSystemFeature::ObjectIDs, nativeFeatures & FILE_SUPPORTS_OBJECT_IDS);
+		Utility::ModFlagRef(features, FileSystemFeature::OpenFileByID, nativeFeatures & FILE_SUPPORTS_OPEN_BY_FILE_ID);
+		Utility::ModFlagRef(features, FileSystemFeature::ReparsePoints, nativeFeatures & FILE_SUPPORTS_REPARSE_POINTS);
+		Utility::ModFlagRef(features, FileSystemFeature::SparseFiles, nativeFeatures & FILE_SUPPORTS_SPARSE_FILES);
+		Utility::ModFlagRef(features, FileSystemFeature::Transactions, nativeFeatures & FILE_SUPPORTS_TRANSACTIONS);
+		Utility::ModFlagRef(features, FileSystemFeature::USNJournal, nativeFeatures & FILE_SUPPORTS_USN_JOURNAL);
+		Utility::ModFlagRef(features, FileSystemFeature::Unicode, nativeFeatures & FILE_UNICODE_ON_DISK);
+		Utility::ModFlagRef(features, FileSystemFeature::CompressedVolume, nativeFeatures & FILE_VOLUME_IS_COMPRESSED);
+		Utility::ModFlagRef(features, FileSystemFeature::VolumeQuotas, nativeFeatures & FILE_VOLUME_QUOTAS);
 
 		return features;
 	}
