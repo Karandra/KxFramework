f7a3080d1c6a78385697f05f4e4c5247ff00fc9a
Kerber <DaIvan97@gmail.com>
2020-03-30 15:30:23 +0300

    Added 'CopyItem' and 'CopyItem' functions

--- Kx/FileSystem/IFileSystem.h
+++ Kx/FileSystem/IFileSystem.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Common.h"
 #include "FSPath.h"
+#include "Kx/General/BinarySize.h"
 #include "Kx/General/UndefWindows.h"
 
 namespace KxFramework
@@ -13,12 +14,22 @@ namespace KxFramework
 
 		CaseSensitive = 1 << 0,
 		Recursive = 1 << 1,
+		LimitToFiles = 1 << 2,
+		LimitToDirectories = 1 << 3,
+	};
+	enum class FSCopyItemFlag
+	{
+		None = 0,
+
+		ReplaceIfExist = 1 << 0,
+		NoBuffering = 1 << 1,
 	};
 }
 
 namespace KxEnumClassOperations
 {
 	KxImplementEnum(KxFramework::FSEnumItemsFlag);
+	KxImplementEnum(KxFramework::FSCopyItemFlag);
 }
 
 namespace KxFramework
@@ -26,17 +37,23 @@ namespace KxFramework
 	class KX_API IFileSystem
 	{
 		public:
+			using TEnumItemsFunc = std::function<bool(FileItem)>;
+			using TCopyItemFunc = std::function<bool(BinarySize, BinarySize)>;
+
+		public:
 			virtual ~IFileSystem() = default;
 
 		public:
 			virtual FileItem GetItem(const FSPath& path) const = 0;
-			virtual size_t EnumItems(const FSPath& directory, std::function<bool(const FileItem&)> func, const wxString& query = {}, FSEnumItemsFlag flags = FSEnumItemsFlag::None) const = 0;
+			virtual size_t EnumItems(const FSPath& directory, TEnumItemsFunc func, const wxString& query = {}, FSEnumItemsFlag flags = FSEnumItemsFlag::None) const = 0;
 			
 			virtual bool CreateDirectory(const FSPath& path) = 0;
 			virtual bool ChangeAttributes(const FSPath& path, FileAttribute attributes) = 0;
 			virtual bool ChangeTimestamp(const FSPath& path, const wxDateTime& creationTime, const wxDateTime& modificationTime, const wxDateTime& lastAccessTime) = 0;
 
-			virtual bool RenameItem(const FSPath& existingPath, const FSPath& newPath, bool replaceIfExist = false) = 0;
+			virtual bool CopyItem(const FSPath& source, const FSPath& destination, TCopyItemFunc func = {}, FSCopyItemFlag flags = FSCopyItemFlag::None) = 0;
+			virtual bool MoveItem(const FSPath& source, const FSPath& destination, TCopyItemFunc func = {}, FSCopyItemFlag flags = FSCopyItemFlag::None) = 0;
+			virtual bool RenameItem(const FSPath& source, const FSPath& destination, FSCopyItemFlag flags = FSCopyItemFlag::None) = 0;
 			virtual bool RemoveItem(const FSPath& path) = 0;
 	};
 }
