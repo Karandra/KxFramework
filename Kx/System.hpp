bee9afc6ec395f96f22b5e711b28dc20a15d53e6
Kerber <DaIvan97@gmail.com>
2020-04-01 04:42:26 +0300

    Added includes

--- Kx/System.hpp
+++ Kx/System.hpp
@@ -2,7 +2,10 @@
 #include "Kx/Common.hpp"
 #include "Kx/System/Common.h"
 
+#include "Kx/System/COM.h"
+#include "Kx/System/ErrorCode.h"
+#include "Kx/System/ErrorCodeValue.h"
 #include "Kx/System/ShellFileType.h"
 #include "Kx/System/ShellFileTypeInfo.h"
 #include "Kx/System/ShellFileTypeManager.h"
-
+#include "Kx/System/ShellOperations.h"
