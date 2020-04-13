476b775b7fc1ce6f2518537b283da57271c63c39
Kerber <DaIvan97@gmail.com>
2020-04-14 01:44:37 +0300

    Added 'TextDocument'

---
+++ Kx/General/TextDocument.cpp
@@ -0,0 +1,130 @@
+#include "KxStdAfx.h"
+#include "TextDocument.h"
+#include "Kx/Utility/CallAtScopeExit.h"
+#include "KxFramework/KxFileStream.h"
+#include <wx/textfile.h>
+
+namespace
+{
+	wxTextFileType MapLineBreakType(KxFramework::TextDocument::LineBreakFormat lineBreakFormat) noexcept
+	{
+		using namespace KxFramework::TextDocument;
+
+		switch (lineBreakFormat)
+		{
+			case LineBreakFormat::Windows:
+			{
+				return wxTextFileType::wxTextFileType_Dos;
+			}
+			case LineBreakFormat::Unix:
+			{
+				return wxTextFileType::wxTextFileType_Unix;
+			}
+			case LineBreakFormat::Mac:
+			{
+				return wxTextFileType::wxTextFileType_Mac;
+			}
+		}
+		return wxTextFileType::wxTextFileType_None;
+	}
+}
+
+namespace KxFramework::TextDocument
+{
+	size_t Read(const FSPath& filePath, std::function<bool(String)> func)
+	{
+		wxTextFile textFile(filePath.GetFullPath());
+		if (textFile.Open())
+		{
+			Utility::CallAtScopeExit atExit([&]()
+			{
+				textFile.Close();
+			});
+
+			size_t count = 0;
+			for (size_t i = 0; i < textFile.GetLineCount(); i++)
+			{
+				count++;
+				if (!std::invoke(func, String(textFile.GetLine(i))))
+				{
+					break;
+				}
+			}
+			return count;
+		}
+		return 0;
+	}
+	String Read(const FSPath& filePath, LineBreakFormat lineBreakFormat)
+	{
+		String result;
+		const wxChar* lineBreak = wxTextFile::GetEOL(MapLineBreakType(lineBreakFormat));
+
+		Read(filePath, [&](String line)
+		{
+			if (!result.IsEmpty())
+			{
+				result += lineBreak;
+			}
+			result += std::move(line);
+
+			return true;
+		});
+		return result;
+	}
+
+	bool Write(const FSPath& filePath, const String& text, bool append, LineBreakFormat lineBreakFormat)
+	{
+		const KxFileStream::Disposition disposition = append ? KxFileStream::Disposition::OpenExisting : KxFileStream::Disposition::CreateAlways;
+		KxFileStream stream(filePath, KxFileStream::Access::Write, disposition, KxFileStream::Share::Read);
+		if (stream.IsOk())
+		{
+			bool isSuccess = true;
+			const wxTextFileType lineBreakFormatWx = MapLineBreakType(lineBreakFormat);
+
+			if (append)
+			{
+				stream.SkipToEnd();
+				isSuccess = stream.WriteStringUTF8(wxTextFile::GetEOL(lineBreakFormatWx));
+			}
+
+			return isSuccess && stream.WriteStringUTF8(wxTextFile::Translate(text, lineBreakFormatWx));
+		}
+		return false;
+	}
+	bool Write(const FSPath& filePath, std::function<String()> func, bool append, LineBreakFormat lineBreakFormat)
+	{
+		const KxFileStream::Disposition disposition = append ? KxFileStream::Disposition::OpenExisting : KxFileStream::Disposition::CreateAlways;
+		KxFileStream stream(filePath, KxFileStream::Access::Write, disposition, KxFileStream::Share::Read);
+		if (stream.IsOk())
+		{
+			const wxTextFileType lineBreakFormatWx = MapLineBreakType(lineBreakFormat);
+			const wxChar* lineBreak = wxTextFile::GetEOL(lineBreakFormatWx);
+
+			bool isSuccess = true;
+			if (append)
+			{
+				stream.SkipToEnd();
+				isSuccess = stream.WriteStringUTF8(lineBreak);
+			}
+
+			while (isSuccess)
+			{
+				String text = std::invoke(func);
+				if (!text.IsEmpty())
+				{
+					isSuccess = stream.WriteStringUTF8(wxTextFile::Translate(text, lineBreakFormatWx));
+					if (isSuccess)
+					{
+						isSuccess = stream.WriteStringUTF8(lineBreak);
+					}
+				}
+				else
+				{
+					break;
+				}
+			}
+			return isSuccess;
+		}
+		return false;
+	}
+}
---
+++ Kx/General/TextDocument.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Common.h"
+#include "String.h"
+#include "Kx/FileSystem/FSPath.h"
+
+namespace KxFramework::TextDocument
+{
+	enum class LineBreakFormat
+	{
+		None = -1,
+
+		Windows, // CR LF (13, 10)
+		Unix, // LF (10)
+		Mac, // CR (13)
+	};
+}
+
+namespace KxFramework::TextDocument
+{
+	size_t Read(const FSPath& filePath, std::function<bool(String)> func);
+	String Read(const FSPath& filePath, LineBreakFormat lineBreakFormat = LineBreakFormat::Windows);
+
+	bool Write(const FSPath& filePath, const String& text, bool append = false, LineBreakFormat lineBreakFormat = LineBreakFormat::Windows);
+	bool Write(const FSPath& filePath, std::function<String()> func, bool append = false, LineBreakFormat lineBreakFormat = LineBreakFormat::Windows);
+}
