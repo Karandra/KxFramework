47d4df2fb3e83183461012e91d8811b6332e6ad4
Kerber <DaIvan97@gmail.com>
2020-04-14 15:25:26 +0300

    Added Drawing module

---
+++ Kx/Drawing/Common.cpp
@@ -0,0 +1,40 @@
+#include "KxStdAfx.h"
+#include "Common.h"
+
+namespace
+{
+	constexpr int g_BitmapDepth = 32;
+	constexpr wxBitmapTransparency g_BitmapTransparency = wxBitmapTransparency::wxBitmapTransparency_Always;
+}
+
+namespace KxFramework::Drawing
+{
+	wxIcon ToIcon(const wxImage& image)
+	{
+		return ToIcon(ToBitmap(image));
+	}
+	wxIcon ToIcon(const wxBitmap& bitmap)
+	{
+		wxIcon icon;
+		icon.CopyFromBitmap(bitmap);
+		return icon;
+	}
+
+	wxImage ToImage(const wxIcon& icon)
+	{
+		return ToImage(ToBitmap(icon));
+	}
+	wxImage ToImage(const wxBitmap& bitmap)
+	{
+		return bitmap.ConvertToImage();
+	}
+
+	wxBitmap ToBitmap(const wxIcon& icon)
+	{
+		return wxBitmap(icon, g_BitmapTransparency);
+	}
+	wxBitmap ToBitmap(const wxImage& image)
+	{
+		return wxBitmap(image, g_BitmapDepth);
+	}
+}
---
+++ Kx/Drawing/Common.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Kx/Common.hpp"
+#include <wx/icon.h>
+#include <wx/image.h>
+#include <wx/bitmap.h>
+#include "Kx/System/UndefWindows.h"
+
+namespace KxFramework::Drawing
+{
+	wxIcon ToIcon(const wxImage& image);
+	wxIcon ToIcon(const wxBitmap& bitmap);
+
+	wxImage ToImage(const wxIcon& icon);
+	wxImage ToImage(const wxBitmap& bitmap);
+
+	wxBitmap ToBitmap(const wxIcon& icon);
+	wxBitmap ToBitmap(const wxImage& image);
+}
