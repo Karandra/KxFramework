0ced1deb7a8c913518a9c18462079263e8f44605
Kerber <DaIvan97@gmail.com>
2020-04-14 17:43:50 +0300

    Removed original KxDataViewCtrl

--- KxFramework/KxDataViewListModelEx.cpp
+++
@@ -1,2 +0,0 @@
-#include "KxStdAfx.h"
-#include "KxFramework/KxDataViewListModelEx.h"
--- KxFramework/KxDataViewModelExBase.cpp
+++
@@ -1,3 +0,0 @@
-#include "KxStdAfx.h"
-#include "KxFramework/KxDataViewModelExBase.h"
-#include "KxFramework/DataView/KxDataViewMainWindow.h"
