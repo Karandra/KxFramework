3bce4de43e6fe99e3dc0ff152afec440e6710349
Kerber <DaIvan97@gmail.com>
2020-04-15 00:28:09 +0300

    WindowUpdateLocker moved to Kx sub-project

---
+++ Kx/UI/WindowUpdateLocker.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "Common.h"
+#include <wx/window.h>
+
+namespace KxFramework
+{
+	class WindowUpdateLocker final
+	{
+		private:
+			wxWindow* m_Window = nullptr;
+
+		public:
+			WindowUpdateLocker() = default;
+			WindowUpdateLocker(wxWindow& window)
+				:m_Window(&window)
+			{
+				m_Window->Freeze();
+			}
+			WindowUpdateLocker(const WindowUpdateLocker&) = delete;
+			WindowUpdateLocker(WindowUpdateLocker&& other)
+			{
+				*this = std::move(other);
+			}
+			~WindowUpdateLocker()
+			{
+				if (m_Window)
+				{
+					m_Window->Thaw();
+				}
+			}
+
+		public:
+			WindowUpdateLocker& operator=(const WindowUpdateLocker&) = delete;
+			WindowUpdateLocker& operator=(WindowUpdateLocker&& other)
+			{
+				m_Window = other.m_Window;
+				other.m_Window = nullptr;
+
+				return *this;
+			}
+	};
+}
