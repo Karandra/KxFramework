b45bfcfc671968cbc57773e6eff531d795b12039
Kerber <DaIvan97@gmail.com>
2020-04-17 02:22:12 +0300

    Removed 'KxMainDefs.h' file

--- KxFramework/KxFramework.h
+++ KxFramework/KxFramework.h
@@ -1,3 +1,13 @@
 #pragma once
 #include "KxIncludes.h"
-#include "KxMainDefs.h"
+
+// Misc
+#define KX_COMCALL STDMETHODCALLTYPE
+
+#if defined KXFRAMEWORK_LIBRARY
+#define KX_API __declspec(dllexport)
+#elif defined KXFRAMEWORK_PLUGIN
+#define KX_API __declspec(dllimport)
+#else
+#define KX_API
+#endif
