f8456886d7a44338dd2f221d5a49ac61053d77cb
Kerber <DaIvan97@gmail.com>
2020-04-17 02:08:56 +0300

    Added 'UI::GetOwnerWindowHandle' function

---
+++ Kx/UI/Common.cpp
@@ -0,0 +1,26 @@
+#include "KxStdAfx.h"
+#include "Common.h"
+#include <wx/window.h>
+
+namespace KxFramework::UI
+{
+	void* GetOwnerWindowHandle(const wxWindow* window) noexcept
+	{
+		if (window)
+		{
+			if (window = wxGetTopLevelParent(const_cast<wxWindow*>(window)))
+			{
+				return window->GetHandle();
+			}
+			else if (window = wxTheApp->GetTopWindow())
+			{
+				return window->GetHandle();
+			}
+			else if (window = wxGetTopLevelParent(wxGetActiveWindow()))
+			{
+				return window->GetHandle();
+			}
+		}
+		return ::GetShellWindow();
+	}
+}
--- Kx/UI/Common.h
+++ Kx/UI/Common.h
@@ -1,2 +1,11 @@
 #pragma once
 #include "Kx/Common.hpp"
+class wxWindow;
+
+namespace KxFramework::UI
+{
+	// Get any valid HWND for functions that requires parent window in all cases.
+	// Return HWND of windows in following chain:
+	// window -> wxTheApp->GetTopWindow() -> wxGetTopLevelParent(wxGetActiveWindow()) -> ::GetShellWindow()
+	void* GetOwnerWindowHandle(const wxWindow* window) noexcept;
+}
