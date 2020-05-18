b3c332b8c72dc638e23b0dccaae540c53331f5c0
Kerber <DaIvan97@gmail.com>
2020-05-18 12:30:52 +0300

    Added 'EventSourceID' typedef

--- Kx/EventSystem/Common.h
+++ Kx/EventSystem/Common.h
@@ -1,10 +1,12 @@
 #pragma once
 #include "Kx/Common.hpp"
 #include <wx/event.h>
+#include <wx/windowid.h>
 
 namespace KxFramework
 {
 	using EventID = wxEventType;
+	using EventSourceID = wxWindowID;
 	
 	template<class T>
 	using EventTag = wxEventTypeTag<T>;
