d487a3cb0fca524441e4cd9cd17b4c0be4504f3a
Kerber <DaIvan97@gmail.com>
2020-05-20 02:52:45 +0300

    Added 'TimeZone' class

---
+++ Kx/General/DateTime/TimeZone.cpp
@@ -0,0 +1,49 @@
+#include "stdafx.h"
+#include "TimeZone.h"
+#include <chrono>
+
+namespace
+{
+	constexpr int g_LocalOffset = -1;
+}
+
+namespace KxFramework
+{
+	bool TimeZoneOffset::IsLocal() const noexcept
+	{
+		return m_Offset.GetMilliseconds() == g_LocalOffset;
+	}
+	TimeSpan TimeZoneOffset::GetOffset() const noexcept
+	{
+		return TimeSpan::Seconds(static_cast<wxDateTime::TimeZone>(*this).GetOffset());
+	}
+
+	TimeZoneOffset& TimeZoneOffset::operator=(TimeZone tz) noexcept
+	{
+		*this = wxDateTime::TimeZone(tz);
+		return *this;
+	}
+	TimeZoneOffset& TimeZoneOffset::operator=(const wxDateTime::TimeZone& other) noexcept
+	{
+		if (other.IsLocal())
+		{
+			m_Offset = TimeSpan::Milliseconds(g_LocalOffset);
+		}
+		else
+		{
+			m_Offset = TimeSpan::Milliseconds(other.GetOffset());
+		}
+		return *this;
+	}
+	TimeZoneOffset::operator wxDateTime::TimeZone() const noexcept
+	{
+		if (IsLocal())
+		{
+			return g_LocalOffset;
+		}
+		else
+		{
+			return m_Offset.GetSeconds();
+		}
+	}
+}
---
+++ Kx/General/DateTime/TimeZone.h
@@ -0,0 +1,49 @@
+#pragma once
+#include "Common.h"
+#include "TimeSpan.h"
+
+namespace KxFramework
+{
+	using TimeZone = wxDateTime::TZ;
+}
+
+namespace KxFramework
+{
+	class KX_API TimeZoneOffset final
+	{
+		private:
+			TimeSpan m_Offset;
+
+		public:
+			TimeZoneOffset(const TimeSpan& offset = {}) noexcept
+				:m_Offset(offset)
+			{
+			}
+			TimeZoneOffset(const wxDateTime::TimeZone& other) noexcept
+			{
+				*this = other;
+			}
+			TimeZoneOffset(TimeZone tz) noexcept
+			{
+				*this = tz;
+			}
+
+		public:
+			bool IsLocal() const noexcept;
+			TimeSpan GetOffset() const noexcept;
+
+		public:
+			TimeZoneOffset& operator=(TimeZone tz) noexcept;
+			TimeZoneOffset& operator=(const wxDateTime::TimeZone& other) noexcept;
+			operator wxDateTime::TimeZone() const noexcept;
+
+			bool operator==(const TimeZoneOffset& other) const noexcept
+			{
+				return m_Offset == other.m_Offset;
+			}
+			bool operator!=(const TimeZoneOffset& other) const noexcept
+			{
+				return !(*this == other);
+			}
+	};
+}
