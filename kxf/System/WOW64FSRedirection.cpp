efd939c0f04c66ec0351b8a0f2ebcb1c8f96e7a6
Kerber <DaIvan97@gmail.com>
2020-05-25 00:00:00 +0300

    Added error check, added move constructor/operator.

--- kxf/System/WOW64FSRedirection.cpp
+++ kxf/System/WOW64FSRedirection.cpp
@@ -8,12 +8,23 @@ namespace kxf
 	{
 		if (NativeAPI::Kernel32::Wow64DisableWow64FsRedirection)
 		{
-			NativeAPI::Kernel32::Wow64DisableWow64FsRedirection(&m_Value);
+			if (NativeAPI::Kernel32::Wow64DisableWow64FsRedirection(&m_Value))
+			{
+				m_ErrorCode = Win32Error::Success();
+			}
+			else
+			{
+				m_ErrorCode = Win32Error::GetLastError();
+			}
+		}
+		else
+		{
+			m_ErrorCode = ERROR_INVALID_FUNCTION;
 		}
 	}
 	DisableWOW64FSRedirection::~DisableWOW64FSRedirection() noexcept
 	{
-		if (NativeAPI::Kernel32::Wow64RevertWow64FsRedirection && m_Value)
+		if (NativeAPI::Kernel32::Wow64RevertWow64FsRedirection && m_ErrorCode.IsSuccess())
 		{
 			NativeAPI::Kernel32::Wow64RevertWow64FsRedirection(&m_Value);
 		}
--- kxf/System/WOW64FSRedirection.h
+++ kxf/System/WOW64FSRedirection.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Common.h"
+#include "ErrorCodeValue.h"
 
 namespace kxf
 {
@@ -7,9 +8,37 @@ namespace kxf
 	{
 		private:
 			void* m_Value = nullptr;
+			Win32Error m_ErrorCode = Win32Error::Fail();
 
 		public:
 			DisableWOW64FSRedirection() noexcept;
+			DisableWOW64FSRedirection(const DisableWOW64FSRedirection&) = delete;
+			DisableWOW64FSRedirection(DisableWOW64FSRedirection&& other)
+			{
+				*this = std::move(other);
+			}
 			~DisableWOW64FSRedirection() noexcept;
+
+		public:
+			explicit operator bool() const noexcept
+			{
+				return m_ErrorCode.IsSuccess();
+			}
+			bool operator!() const noexcept
+			{
+				return m_ErrorCode.IsFail();
+			}
+
+			DisableWOW64FSRedirection& operator=(const DisableWOW64FSRedirection&) = delete;
+			DisableWOW64FSRedirection& operator=(DisableWOW64FSRedirection&& other) noexcept
+			{
+				m_Value = other.m_Value;
+				other.m_Value = nullptr;
+
+				m_ErrorCode = other.m_ErrorCode;
+				other.m_ErrorCode = Win32Error::Fail();
+
+				return *this;
+			}
 	};
 }
