b34ede4b4e4469d706357bb25945f7d52f09624a
Kerber <DaIvan97@gmail.com>
2020-07-22 18:56:10 +0300

    Using 'Utility::WithOptionalOwnership'

--- kxf/EventSystem/EventItem.h
+++ kxf/EventSystem/EventItem.h
@@ -11,55 +11,35 @@ namespace kxf::EventSystem
 	class EventItem final
 	{
 		private:
-			IEventExecutor* m_Executor = nullptr;
-			bool m_ExecutorOwn = false;
-
+			Utility::WithOptionalOwnership<IEventExecutor> m_Executor;
 			EventID m_EventID;
 			LocallyUniqueID m_BindSlot;
 			FlagSet<EventFlag> m_Flags;
 
-		private:
-			void Destroy() noexcept
-			{
-				if (m_ExecutorOwn)
-				{
-					delete m_Executor;
-				}
-				m_Executor = nullptr;
-				m_ExecutorOwn = false;
-			}
-
 		public:
 			EventItem() noexcept = default;
 			EventItem(EventID eventID, std::unique_ptr<IEventExecutor> executor) noexcept
-				:m_Executor(executor.release()), m_EventID(eventID), m_ExecutorOwn(true)
+				:m_Executor(std::move(executor)), m_EventID(eventID)
 			{
 			}
 			EventItem(EventID eventID, IEventExecutor& executor) noexcept
-				:m_Executor(&executor), m_EventID(eventID), m_ExecutorOwn(false)
+				:m_Executor(executor), m_EventID(eventID)
 			{
 			}
-			EventItem(EventItem&& other) noexcept
-			{
-				*this = std::move(other);
-			}
+			EventItem(EventItem&&) noexcept = default;
 			EventItem(const EventItem&) = delete;
-			~EventItem() noexcept
-			{
-				Destroy();
-			}
 
 		public:
 			bool IsNull() const noexcept
 			{
-				return m_Executor == nullptr || m_EventID.IsNull();
+				return m_Executor.IsNull() || m_EventID.IsNull();
 			}
 			bool IsSameAs(const EventItem& other) const noexcept
 			{
 				if (this != &other)
 				{
 					const bool sameID = m_EventID == other.m_EventID;
-					return sameID && (m_Executor == other.m_Executor || (m_Executor && other.m_Executor && m_Executor->IsSameAs(*other.m_Executor)));
+					return sameID && (m_Executor.IsSame(other.m_Executor) || (m_Executor && other.m_Executor && m_Executor->IsSameAs(*other.m_Executor)));
 				}
 				return true;
 			}
@@ -70,40 +50,23 @@ namespace kxf::EventSystem
 
 			const IEventExecutor* GetExecutor() const& noexcept
 			{
-				return m_Executor;
+				return m_Executor.Get();
 			}
 			IEventExecutor* GetExecutor() & noexcept
 			{
-				return m_Executor;
+				return m_Executor.Get();
 			}
 			Utility::WithOptionalOwnership<IEventExecutor> GetExecutor() &&
 			{
-				Utility::WithOptionalOwnership<IEventExecutor> ptr;
-				if (m_Executor)
-				{
-					if (m_ExecutorOwn)
-					{
-						ptr.Assign(std::unique_ptr<IEventExecutor>(Utility::ExchangeResetAndReturn(m_Executor, nullptr)));
-						m_ExecutorOwn = false;
-					}
-					else
-					{
-						ptr.Assign(*Utility::ExchangeResetAndReturn(m_Executor, nullptr));
-					}
-				}
-				return ptr;
+				return std::move(m_Executor);
 			}
 			void SetExecutor(std::unique_ptr<IEventExecutor> executor)
 			{
-				Destroy();
-				m_Executor = executor.release();
-				m_ExecutorOwn = true;
+				m_Executor = std::move(executor);
 			}
 			void SetExecutor(IEventExecutor& executor)
 			{
-				Destroy();
-				m_Executor = &executor;
-				m_ExecutorOwn = false;
+				m_Executor = executor;
 			}
 
 			EventID GetEventID() const noexcept
@@ -134,17 +97,7 @@ namespace kxf::EventSystem
 			}
 
 		public:
-			EventItem& operator=(EventItem&& other) noexcept
-			{
-				Destroy();
-
-				m_Executor = Utility::ExchangeResetAndReturn(other.m_Executor, nullptr);
-				m_ExecutorOwn = Utility::ExchangeResetAndReturn(other.m_ExecutorOwn, false);
-				m_EventID = std::move(other.m_EventID);
-				m_BindSlot = std::move(other.m_BindSlot);
-
-				return *this;
-			}
+			EventItem& operator=(EventItem&&) noexcept = default;
 			EventItem& operator=(const EventItem&) = delete;
 
 			explicit operator bool() const noexcept
