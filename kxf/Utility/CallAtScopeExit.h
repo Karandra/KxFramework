c41e2f9e324fd72c4e423cc22fc336e500929d7a
Kerber <DaIvan97@gmail.com>
2020-07-28 23:55:47 +0300

    Disabled copy and move operations

--- kxf/Utility/CallAtScopeExit.h
+++ kxf/Utility/CallAtScopeExit.h
@@ -14,9 +14,15 @@ namespace kxf::Utility
 				:m_Func(std::forward<TFunc>(func))
 			{
 			}
+			CallAtScopeExit(CallAtScopeExit&&) = delete;
+			CallAtScopeExit(const CallAtScopeExit&) = delete;
 			~CallAtScopeExit() noexcept
 			{
 				std::invoke(m_Func);
 			}
+
+		public:
+			CallAtScopeExit& operator=(CallAtScopeExit&&) = delete;
+			CallAtScopeExit& operator=(const CallAtScopeExit&) = delete;
 	};
 }
