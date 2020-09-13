cd9396dac39b2fa9375d78ddd27037553e841b72
Kerber <daivan97@gmail.com>
2020-09-13 18:30:12 +0300

    Added 'QueryInt/String' functions

--- kxf/General/ResourceID.h
+++ kxf/General/ResourceID.h
@@ -12,26 +12,26 @@ namespace kxf
 
 		public:
 			ResourceID() noexcept = default;
-			ResourceID(int id) noexcept
-				:m_ID(id)
-			{
-			}
-			ResourceID(String id) noexcept
-				:m_ID(id)
-			{
-			}
-			ResourceID(const char* id) noexcept
-				:m_ID(String(id))
-			{
-			}
-			ResourceID(const wchar_t* id) noexcept
-				:m_ID(String(id))
+
+			template<class T, class = std::enable_if_t<std::is_integral_v<T> || std::is_enum_v<T>>>
+			ResourceID(T id) noexcept
+				:m_ID(static_cast<int>(id))
 			{
 			}
 
-			template<class T, class = std::enable_if_t<std::is_enum_v<T>>>
-			ResourceID(T id) noexcept
-				:m_ID(static_cast<int>(id))
+			ResourceID(String id) noexcept
+			{
+				if (!id.IsEmptyOrWhitespace())
+				{
+					m_ID = std::move(id);
+				}
+			}
+			ResourceID(const char* id) noexcept
+				:ResourceID(String(id))
+			{
+			}
+			ResourceID(const wchar_t* id) noexcept
+				:ResourceID(String(id))
 			{
 			}
 
@@ -55,39 +55,6 @@ namespace kxf
 				}
 				return false;
 			}
-
-			int GetInt() const noexcept
-			{
-				if (auto value = std::get_if<int>(&m_ID))
-				{
-					return *value;
-				}
-				return 0;
-			}
-
-			template<class T, class = std::enable_if_t<std::is_enum_v<T>>>
-			T GetInt() const noexcept
-			{
-				return static_cast<T>(GetInt());
-			}
-
-			const String& GetString() const& noexcept
-			{
-				if (auto value = std::get_if<String>(&m_ID))
-				{
-					return *value;
-				}
-				return NullString;
-			}
-			String GetString() && noexcept
-			{
-				if (auto value = std::get_if<String>(&m_ID))
-				{
-					return std::move(*value);
-				}
-				return {};
-			}
-
 			size_t GetHash() const noexcept
 			{
 				if (auto value = std::get_if<int>(&m_ID))
@@ -99,6 +66,62 @@ namespace kxf
 					return std::hash<String>()(*value);
 				}
 				return 0;
+			}
+
+			// Integer
+			template<class T = int, class = std::enable_if_t<std::is_integral_v<T> || std::is_enum_v<T>>>
+			T GetInt() const noexcept
+			{
+				if (auto value = std::get_if<int>(&m_ID))
+				{
+					return static_cast<T>(*value);
+				}
+				return 0;
+			}
+
+			template<class T = int, class = std::enable_if_t<std::is_integral_v<T> || std::is_enum_v<T>>>
+			std::optional<T> QueryInt() const noexcept
+			{
+				if (auto value = std::get_if<int>(&m_ID))
+				{
+					return static_cast<T>(*value);
+				}
+				return {};
+			}
+
+			// String
+			String GetString() const& noexcept
+			{
+				if (auto value = std::get_if<String>(&m_ID))
+				{
+					return *value;
+				}
+				return {};
+			}
+			String GetString() && noexcept
+			{
+				if (auto value = std::get_if<String>(&m_ID))
+				{
+					return std::move(*value);
+				}
+				return {};
+			}
+
+			std::optional<String> QueryString() const&
+			{
+				if (auto value = std::get_if<String>(&m_ID))
+				{
+					return *value;
+				}
+				return {};
+			}
+			std::optional<String> QueryString() && noexcept
+			{
+				if (auto value = std::get_if<String>(&m_ID))
+				{
+					return std::move(*value);
+				}
+				return {};
 			}
 
 		public:
