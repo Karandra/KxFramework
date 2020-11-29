d782b2da6dffdf3bfdb08198f01554f9eea5878e
Kerber <DaIvan97@gmail.com>
2020-11-29 18:21:57 +0300

    Added modified portfile for wxWidgets

---
+++ VCPkg/ports/wxwidgets/portfile.cmake
@@ -0,0 +1,88 @@
+vcpkg_fail_port_install(ON_TARGET "uwp")
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO wxWidgets/wxWidgets
+    REF v3.1.4
+    SHA512 108e35220de10afbfc58762498ada9ece0b3166f56a6d11e11836d51bfbaed1de3033c32ed4109992da901fecddcf84ce8a1ba47303f728c159c638dac77d148
+    HEAD_REF master
+    PATCHES
+        disable-platform-lib-dir.patch
+        fix-stl-build-vs2019-16.6.patch
+        enable-d3d11.patch
+)
+
+set(OPTIONS)
+if(VCPKG_TARGET_IS_OSX)
+    set(OPTIONS -DCOTIRE_MINIMUM_NUMBER_OF_TARGET_SOURCES=9999)
+endif()
+
+if(VCPKG_TARGET_ARCHITECTURE STREQUAL arm64 OR VCPKG_TARGET_ARCHITECTURE STREQUAL arm)
+    set(OPTIONS
+        -DwxUSE_OPENGL=OFF
+        -DwxUSE_STACKWALKER=OFF
+    )
+endif()
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
+        -DwxUSE_REGEX=builtin
+        -DwxUSE_ZLIB=sys
+        -DwxUSE_EXPAT=sys
+        -DwxUSE_LIBJPEG=sys
+        -DwxUSE_LIBPNG=sys
+        -DwxUSE_LIBTIFF=sys
+		
+        -DwxUSE_STL=ON
+        -DwxUSE_STD_DEFAULT=ON
+        -DWXWIN_COMPATIBILITY_2_8=OFF
+        -DWXWIN_COMPATIBILITY_3_0=OFF
+        -DwxDIALOG_UNIT_COMPATIBILITY=OFF
+        -DwxUSE_UNSAFE_WXSTRING_CONV=OFF
+        -DwxNO_UNSAFE_WXSTRING_CONV=ON
+        -DwxUSE_INTL=OFF
+        -DwxUSE_IPV6=ON
+        -DwxUSE_WINSOCK2=ON
+		
+        -DwxBUILD_DISABLE_PLATFORM_LIB_DIR=ON
+        ${OPTIONS}
+)
+
+vcpkg_install_cmake()
+
+vcpkg_copy_tools(TOOL_NAMES wxrc AUTO_CLEAN)
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
+
+file(GLOB DLLS "${CURRENT_PACKAGES_DIR}/lib/*.dll")
+if(DLLS)
+    file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/bin)
+    foreach(DLL ${DLLS})
+        get_filename_component(N "${DLL}" NAME)
+        file(RENAME ${DLL} ${CURRENT_PACKAGES_DIR}/bin/${N})
+    endforeach()
+endif()
+file(GLOB DLLS "${CURRENT_PACKAGES_DIR}/debug/lib/*.dll")
+if(DLLS)
+    file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/bin)
+    foreach(DLL ${DLLS})
+        get_filename_component(N "${DLL}" NAME)
+        file(RENAME ${DLL} ${CURRENT_PACKAGES_DIR}/debug/bin/${N})
+    endforeach()
+endif()
+
+# do the copy pdbs now after the dlls got moved to the expected /bin folder above
+vcpkg_copy_pdbs()
+
+if(EXISTS ${CURRENT_PACKAGES_DIR}/lib/mswu/wx/setup.h)
+    file(RENAME ${CURRENT_PACKAGES_DIR}/lib/mswu/wx/setup.h ${CURRENT_PACKAGES_DIR}/include/wx/setup.h)
+endif()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib/mswu)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/lib/mswud)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/include/msvc)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+
+file(INSTALL ${SOURCE_PATH}/docs/licence.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
