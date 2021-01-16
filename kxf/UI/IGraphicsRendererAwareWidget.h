ffd83a70630c58ba5917bef020791b93802bffe3
Kerber <DaIvan97@gmail.com>
2021-01-16 17:29:11 +0300

    Added 'IGraphicsRendererAwareWidget' interface

---
+++ kxf/UI/IGraphicsRendererAwareWidget.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Common.h"
+
+namespace kxf
+{
+	class IGraphicsRenderer;
+}
+
+namespace kxf::UI
+{
+	class IGraphicsRendererAwareWidget: public RTTI::Interface<IGraphicsRendererAwareWidget>
+	{
+		KxRTTI_DeclareIID(IGraphicsRendererAwareWidget, {0x3dc68a96, 0xa7fe, 0x479d, {0xb0, 0x4, 0x14, 0x37, 0xf4, 0x62, 0xd4, 0xed}});
+
+		public:
+			virtual ~IGraphicsRendererAwareWidget() = default;
+
+		public:
+			virtual std::shared_ptr<IGraphicsRenderer> GetActiveGraphicsRenderer() const = 0;
+			virtual void SetActiveGraphicsRenderer(std::shared_ptr<IGraphicsRenderer> renderer) = 0;
+	};
+}
