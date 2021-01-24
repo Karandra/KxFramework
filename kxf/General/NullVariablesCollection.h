f66a5ac1beabf079fb6fb39f2b621f61aca0c3bd
Kerber <DaIvan97@gmail.com>
2021-01-24 16:38:27 +0300

    Added 'NullVariablesCollection' class

---
+++ kxf/General/NullVariablesCollection.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "Common.h"
+#include "IVariablesCollection.h"
+#include "kxf/Utility/Container.h"
+
+namespace kxf
+{
+	class KX_API NullVariablesCollection final: public IVariablesCollection
+	{
+		protected:
+			// IVariablesCollection
+			size_t DoClearItems(const String& ns) override
+			{
+				return 0;
+			}
+			size_t DoGetItemCount(const String& ns) const override
+			{
+				return 0;
+			}
+			size_t DoEnumItems(std::function<bool(const String& ns, const String& id, Any value)> func) const override
+			{
+				return 0;
+			}
+
+			bool DoHasItem(const String& ns, const String& id) const override
+			{
+				return false;
+			}
+			Any DoGetItem(const String& ns, const String& id) const override
+			{
+				return {};
+			}
+			void DoSetItem(const String& ns, const String& id, Any item) override
+			{
+			}
+
+		public:
+			NullVariablesCollection() = default;
+	};
+}
