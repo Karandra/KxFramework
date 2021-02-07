2292ee2730f4731eefe224e4bfee9ba01848e0c6
Kerber <DaIvan97@gmail.com>
2021-02-07 22:17:38 +0300

    Removed 'AbstractEnumerator' and merged it back with the template class

--- kxf/General/Enumerator.h
+++ kxf/General/Enumerator.h
@@ -2,91 +2,6 @@
 #include "Common.h"
 #include "IEnumerator.h"
 
-namespace kxf
-{
-	class AbstractEnumerator: public RTTI::Implementation<AbstractEnumerator, IEnumerator>
-	{
-		protected:
-			static inline constexpr size_t npos = std::numeric_limits<size_t>::max();
-
-		protected:
-			size_t m_Index = 0;
-			size_t m_TotalCount = npos;
-			bool m_IsReset = true;
-
-		private:
-			bool DoMoveNext() noexcept;
-
-		protected:
-			virtual bool InvokeGenerator() = 0;
-
-		protected:
-			AbstractEnumerator(std::optional<size_t> count = {}) noexcept
-				:m_TotalCount(count.value_or(npos))
-			{
-			}
-			AbstractEnumerator(const AbstractEnumerator&) = default;
-			AbstractEnumerator(AbstractEnumerator&&) noexcept = default;
-			virtual ~AbstractEnumerator() = default;
-
-		public:
-			// IEnumerator
-			bool MoveNext() noexcept override
-			{
-				bool result = DoMoveNext();
-				m_IsReset = !result;
-
-				return result;
-			}
-			size_t GetCurrentStep() const noexcept override
-			{
-				return m_Index;
-			}
-
-			bool IsReset() const noexcept override
-			{
-				return m_IsReset || m_Index == npos;
-			}
-			void Reset() noexcept override
-			{
-				m_Index = 0;
-				m_IsReset = true;
-			}
-
-			// GeneratorCommon
-			std::optional<size_t> GetTotalCount() noexcept
-			{
-				if (m_TotalCount != npos)
-				{
-					return m_TotalCount;
-				}
-				return {};
-			}
-
-		public:
-			explicit operator bool() const noexcept
-			{
-				return !IsReset();
-			}
-			bool operator!() const noexcept
-			{
-				return IsReset();
-			}
-
-			bool operator==(const AbstractEnumerator& other) const noexcept
-			{
-				return (this == &other && m_Index == other.m_Index) || m_IsReset && other.m_IsReset;
-			}
-			bool operator!=(const AbstractEnumerator& other) const noexcept
-			{
-				return !(*this == other);
-			}
-
-			AbstractEnumerator& operator=(const AbstractEnumerator&) = default;
-			AbstractEnumerator& operator=(AbstractEnumerator&&) noexcept = default;
-	};
-}
-
 namespace kxf::Private
 {
 	template<class TEnumerator>
@@ -159,19 +74,44 @@ namespace kxf::Private
 namespace kxf
 {
 	template<class TValue_>
-	class Enumerator final: public AbstractEnumerator
+	class Enumerator final: public IEnumerator
 	{
 		public:
 			using TValue = TValue_;
 			using iterator = Private::EnumIterator<Enumerator>;
 
 		private:
+			static inline constexpr size_t npos = std::numeric_limits<size_t>::max();
+
+		private:
 			std::function<std::optional<TValue>(IEnumerator&)> m_MoveNext;
 			std::optional<TValue> m_Value;
+
+			size_t m_Index = 0;
+			size_t m_TotalCount = npos;
+			bool m_IsReset = true;
 			bool m_TerminationRequested = false;
 
-		protected:
-			bool InvokeGenerator() override
+		private:
+			bool DoMoveNext()
+			{
+				if (m_Index == npos || m_Index >= m_TotalCount)
+				{
+					return false;
+				}
+
+				if (InvokeProducer())
+				{
+					m_Index++;
+					return true;
+				}
+				else
+				{
+					m_Index = npos;
+					return false;
+				}
+			}
+			bool InvokeProducer()
 			{
 				m_Value = std::invoke(m_MoveNext, static_cast<IEnumerator&>(*this));
 				return !m_TerminationRequested && m_Value.has_value();
@@ -183,14 +123,14 @@ namespace kxf
 			// std::optional<TValue> func(IEnumerator&);
 			template<class TFunc, std::enable_if_t<std::is_same_v<std::invoke_result_t<TFunc, IEnumerator&>, std::optional<TValue>>, int> = 0>
 			Enumerator(TFunc&& func, std::optional<size_t> count = {}) noexcept
-				:AbstractEnumerator(std::move(count)), m_MoveNext(std::forward<TFunc>(func))
+				:m_TotalCount(count.value_or(npos)), m_MoveNext(std::forward<TFunc>(func))
 			{
 			}
 
 			// std::optional<TValue> func(void);
 			template<class TFunc, std::enable_if_t<std::is_same_v<std::invoke_result_t<TFunc>, std::optional<TValue>>, int> = 0>
 			Enumerator(TFunc&& func, std::optional<size_t> count = {}) noexcept
-				:AbstractEnumerator(std::move(count))
+				:m_TotalCount(count.value_or(npos))
 			{
 				m_MoveNext = [func = std::forward<TFunc>(func)](IEnumerator& enumerator) mutable -> std::optional<TValue>
 				{
@@ -201,7 +141,7 @@ namespace kxf
 			// TValue func(IEnumerator&);
 			template<class TFunc, std::enable_if_t<std::is_same_v<std::invoke_result_t<TFunc, IEnumerator&>, TValue>, int> = 0>
 			Enumerator(TFunc&& func, std::optional<size_t> count = {}) noexcept
-				:AbstractEnumerator(std::move(count))
+				:m_TotalCount(count.value_or(npos))
 			{
 				m_MoveNext = [func = std::forward<TFunc>(func)](IEnumerator& enumerator) mutable -> std::optional<TValue>
 				{
@@ -212,7 +152,7 @@ namespace kxf
 			// TValue func(void);
 			template<class TFunc, std::enable_if_t<std::is_same_v<std::invoke_result_t<TFunc>, TValue>, int> = 0>
 			Enumerator(TFunc&& func, std::optional<size_t> count = {}) noexcept
-				:AbstractEnumerator(std::move(count))
+				:m_TotalCount(count.value_or(npos))
 			{
 				wxASSERT_MSG(count.has_value() && *count != npos, "Producer function with no way to signal termination must only be used with known total limit");
 
@@ -227,17 +167,42 @@ namespace kxf
 
 		public:
 			// IEnumerator
+			bool MoveNext() override
+			{
+				bool result = DoMoveNext();
+				m_IsReset = !result;
+
+				return result;
+			}
 			void Terminate() noexcept override
 			{
 				m_TerminationRequested = true;
 			}
+			size_t GetCurrentStep() const noexcept override
+			{
+				return m_Index;
+			}
+			std::optional<size_t> GetTotalCount() const noexcept override
+			{
+				if (m_TotalCount != npos)
+				{
+					return m_TotalCount;
+				}
+				return {};
+			}
+
+			bool IsReset() const noexcept override
+			{
+				return m_IsReset || m_Index == npos;
+			}
 			void Reset() noexcept override
 			{
-				AbstractEnumerator::Reset();
+				m_Index = 0;
+				m_IsReset = true;
 				m_TerminationRequested = false;
 			}
 
-			// GeneratorOf<TValue>
+			// Enumerator
 			const TValue& GetValue() const& noexcept
 			{
 				return *m_Value;
@@ -262,5 +227,18 @@ namespace kxf
 			{
 				return {};
 			}
+
+		public:
+			bool operator==(const Enumerator& other) const noexcept
+			{
+				return (this == &other && m_Index == other.m_Index) || m_IsReset && other.m_IsReset;
+			}
+			bool operator!=(const Enumerator& other) const noexcept
+			{
+				return !(*this == other);
+			}
+
+			Enumerator& operator=(const Enumerator&) = default;
+			Enumerator& operator=(Enumerator&&) noexcept = default;
 	};
 }
--- kxf/General/IEnumerator.h
+++ kxf/General/IEnumerator.h
@@ -9,9 +9,10 @@ namespace kxf
 		KxRTTI_DeclareIID(IEnumerator, {0x6d4aed72, 0x3c54, 0x4541, {0x9c, 0x2a, 0x57, 0x7a, 0x94, 0x3d, 0x12, 0x73}});
 
 		public:
-			virtual bool MoveNext() noexcept = 0;
+			virtual bool MoveNext() = 0;
 			virtual void Terminate() noexcept = 0;
 			virtual size_t GetCurrentStep() const noexcept = 0;
+			virtual std::optional<size_t> GetTotalCount() const noexcept = 0;
 
 			virtual bool IsReset() const noexcept = 0;
 			virtual void Reset() noexcept = 0;
