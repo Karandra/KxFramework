2e3b1503670228b208b23a7712280501897339e1
Kerber <DaIvan97@gmail.com>
2021-02-08 03:16:14 +0300

    Literal operators are now in their own namespace

--- kxf/Utility/Literals.h
+++ kxf/Utility/Literals.h
@@ -1,11 +1,16 @@
 #pragma once
 #include <cstdint>
 
-namespace kxf
+namespace kxf::Literals
 {
 	// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2018/p0330r3.html
-	constexpr size_t operator "" _zu(unsigned long long value) noexcept
+	constexpr size_t operator""_zu(unsigned long long value) noexcept
 	{
 		return static_cast<size_t>(value);
 	}
+}
+
+namespace kxf
+{
+	using namespace Literals;
 }
