65a910e71c1ad6e12c871155981075b4ee68a523
Kerber <DaIvan97@gmail.com>
2021-02-16 21:59:00 +0300

    Added SBO for 'unique_function' class

--- kxf/General/UniqueFunction.h
+++ kxf/General/UniqueFunction.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Common.h"
+#include "kxf/Utility/Memory.h"
 
 namespace kxf::Private
 {
@@ -12,6 +13,10 @@ namespace kxf::Private
 		public:
 			virtual R invoke(Args&&... arg) const = 0;
 			virtual const std::type_info& target_type() const = 0;
+
+			virtual void move_storage(void* address, unique_function_target_interface&& other) noexcept = 0;
+			virtual void destroy_storage() noexcept = 0;
+
 	};
 
 	template<class F, class R, class... Args>
@@ -36,6 +41,15 @@ namespace kxf::Private
 			{
 				return typeid(F);
 			}
+
+			void move_storage(void* address, unique_function_target_interface<R, Args...>&& other) noexcept override
+			{
+				Utility::ConstructAt<unique_function_target_impl>(address, std::move(static_cast<unique_function_target_impl&>(other)));
+			}
+			void destroy_storage() noexcept override
+			{
+				std::destroy_at(this);
+			}
 	};
 }
 
@@ -52,14 +66,18 @@ namespace kxf
 			using result_type = R;
 
 		private:
-			template<class T>
-			using impl_t = Private::unique_function_target_impl<T, R, Args...>;
+			using interface_t = Private::unique_function_target_interface<R, Args...>;
+
+			template<class TFunc>
+			using impl_t = Private::unique_function_target_impl<TFunc, R, Args...>;
 
 			template<class TFunc>
 			using is_unique_function = std::is_same<std::decay_t<TFunc>, unique_function>;
 
 		private:
-			std::unique_ptr<Private::unique_function_target_interface<R, Args...>> m_Target;
+			std::array<uint8_t, sizeof(void*) * 6> m_LocalBuffer;
+			std::unique_ptr<interface_t> m_HeapBuffer;
+			interface_t* m_Target = nullptr;
 
 		private:
 			template<class TFunc>
@@ -69,29 +87,74 @@ namespace kxf
 			}
 
 			template<class TFunc>
-			static auto CreateTarget(TFunc&& func)
+			interface_t* CreateTarget(TFunc&& func)
 			{
 				using Fx = std::decay_t<TFunc>;
 				using Ix = impl_t<Fx>;
 
-				return std::make_unique<Ix>(std::forward<TFunc>(func));
+				void* address = m_LocalBuffer.data();
+				size_t size = m_LocalBuffer.size();
+				if (std::align(alignof(Ix), sizeof(Ix), address, size))
+				{
+					m_Target = Utility::ConstructAt<Ix>(address, std::forward<TFunc>(func));
+				}
+				else
+				{
+					m_HeapBuffer = std::make_unique<Ix>(std::forward<TFunc>(func));
+					m_Target = m_HeapBuffer.get();
+				}
+				return m_Target;
+			}
+
+			void DestroyTarget()
+			{
+				if (IsUsingHeapBuffer())
+				{
+					m_HeapBuffer.reset();
+				}
+				else if (IsUsingLocalBuffer())
+				{
+					m_Target->destroy_storage();
+				}
+				m_Target = nullptr;
 			}
 
 			template<class T>
 			T* GetTargetImpl() const noexcept
 			{
-				return dynamic_cast<impl_t<T>*>(m_Target.get());
+				return dynamic_cast<impl_t<T>*>(m_Target);
+			}
+
+			bool IsUsingHeapBuffer() const noexcept
+			{
+				return m_Target != nullptr && m_HeapBuffer != nullptr;
+			}
+			bool IsUsingLocalBuffer() const noexcept
+			{
+				return m_Target != nullptr && m_HeapBuffer == nullptr;
+			}
+			size_t GetLocalBufferOffset() const noexcept
+			{
+				return reinterpret_cast<size_t>(m_Target) - reinterpret_cast<size_t>(m_LocalBuffer.data());
 			}
 
 		public:
 			unique_function() = default;
-			unique_function(unique_function&&) noexcept = default;
+			unique_function(unique_function&& other) noexcept
+			{
+				assign(std::move(other));
+			}
 			unique_function(const unique_function&) = delete;
 
 			template<class TFunc, std::enable_if_t<CheckTargetType<TFunc>(), int> = 0>
 			unique_function(TFunc&& func)
-				:m_Target(CreateTarget(std::forward<TFunc>(func)))
 			{
+				CreateTarget(std::forward<TFunc>(func));
+			}
+
+			~unique_function()
+			{
+				DestroyTarget();
 			}
 
 		public:
@@ -110,10 +173,33 @@ namespace kxf
 				return m_Target->invoke(std::forward<Args>(arg)...);
 			}
 
+			void assign(unique_function&& other) noexcept
+			{
+				if (this != &other)
+				{
+					DestroyTarget();
+
+					if (other.IsUsingHeapBuffer())
+					{
+						m_HeapBuffer = std::move(other.m_HeapBuffer);
+						m_Target = m_HeapBuffer.get();
+
+						other.m_Target = nullptr;
+					}
+					else if (other.IsUsingLocalBuffer())
+					{
+						m_Target = std::launder(reinterpret_cast<interface_t*>(m_LocalBuffer.data() + other.GetLocalBufferOffset()));
+						other.m_Target->move_storage(m_Target, std::move(*other.m_Target));
+					}
+					other.DestroyTarget();
+				}
+			}
+
 			template<class TFunc, std::enable_if_t<CheckTargetType<TFunc>(), int> = 0>
 			void assign(TFunc&& func)
 			{
-				m_Target = CreateTarget(std::forward<TFunc>(func));
+				DestroyTarget();
+				CreateTarget(std::forward<TFunc>(func));
 			}
 
 			template<class T>
@@ -152,7 +238,11 @@ namespace kxf
 				return invoke(std::forward<Args>(arg)...);
 			}
 
-			unique_function& operator=(unique_function&&) noexcept = default;
+			unique_function& operator=(unique_function&& other) noexcept
+			{
+				assign(std::move(other));
+				return *this;
+			}
 			unique_function& operator=(const unique_function&) = delete;
 
 			template<class TFunc, std::enable_if_t<CheckTargetType<TFunc>(), int> = 0>
