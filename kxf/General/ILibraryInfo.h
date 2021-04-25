d3c33d7c797046c4028de9b3624674891a787b95
Kerber <DaIvan97@gmail.com>
2021-04-25 16:07:20 +0300

    Added 'ILibraryInfo' interface

---
+++ kxf/General/ILibraryInfo.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Common.h"
+#include "String.h"
+#include "Version.h"
+#include "kxf/RTTI/RTTI.h"
+
+namespace kxf
+{
+	class ILibraryInfo: public RTTI::Interface<ILibraryInfo>
+	{
+		KxRTTI_DeclareIID(ILibraryInfo, {0x66ffd9cb, 0x32cc, 0x47dd, {0xab, 0x37, 0xb0, 0x83, 0xf2, 0x61, 0x3e, 0xc1}});
+
+		public:
+			virtual String GetName() const = 0;
+			virtual Version GetVersion() const = 0;
+			virtual uint32_t GetAPILevel() const = 0;
+	};
+}
