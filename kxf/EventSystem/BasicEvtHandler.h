2d837a9c109d77b6a6ea89f9575d35a5f6a888fc
Kerber <DaIvan97@gmail.com>
2021-06-29 03:32:18 +0300

    BasicEvtHandler turned into a proper CRTP template class

--- kxf/EventSystem/BasicEvtHandler.h
+++ kxf/EventSystem/BasicEvtHandler.h
@@ -4,103 +4,114 @@
 
 namespace kxf
 {
-	template<class TBase, class TEvtHandler = EvtHandler>
-	class BasicEvtHandler: public RTTI::PrivateStub<BasicEvtHandler<TBase, TEvtHandler>, TBase>
+	template<class TDerived, class TBase>
+	class BasicEvtHandler: public RTTI::PrivateStub<BasicEvtHandler<TDerived, TBase>, TBase>
 	{
-		protected:
-			TEvtHandler m_EvtHandler;
+		private:
+			TDerived& AsDerived() noexcept
+			{
+				return static_cast<TDerived&>(*this);
+			}
+			const TDerived& AsDerived() const noexcept
+			{
+				return static_cast<const TDerived&>(*this);
+			}
 
 		protected:
-			EventSystem::EvtHandlerAccessor AccessEvtHandler() noexcept
+			EventSystem::EvtHandlerAccessor AccessThisEvtHandler() noexcept
 			{
-				return m_EvtHandler;
+				return AsDerived().GetThisEvtHandler();
+			}
+			EventSystem::EvtHandlerAccessor AccessTopEvtHandler() noexcept
+			{
+				return AsDerived().GetTopEvtHandler();
 			}
 
 			// IEvtHandler
 			LocallyUniqueID DoBind(const EventID& eventID, std::unique_ptr<IEventExecutor> executor, FlagSet<BindEventFlag> flags = {}) override
 			{
-				return AccessEvtHandler().DoBind(eventID, std::move(executor), flags);
+				return AccessThisEvtHandler().DoBind(eventID, std::move(executor), flags);
 			}
 			bool DoUnbind(const EventID& eventID, IEventExecutor& executor) override
 			{
-				return AccessEvtHandler().DoUnbind(eventID, executor);
+				return AccessThisEvtHandler().DoUnbind(eventID, executor);
 			}
 			bool DoUnbind(const LocallyUniqueID& bindSlot) override
 			{
-				return AccessEvtHandler().DoUnbind(bindSlot);
+				return AccessThisEvtHandler().DoUnbind(bindSlot);
 			}
 
 			bool OnDynamicBind(EventSystem::EventItem& eventItem) override
 			{
-				return AccessEvtHandler().OnDynamicBind(eventItem);
+				return AccessThisEvtHandler().OnDynamicBind(eventItem);
 			}
 			bool OnDynamicUnbind(EventSystem::EventItem& eventItem) override
 			{
-				return AccessEvtHandler().OnDynamicUnbind(eventItem);
+				return AccessThisEvtHandler().OnDynamicUnbind(eventItem);
 			}
 
 			std::unique_ptr<IEvent> DoQueueEvent(std::unique_ptr<IEvent> event, const EventID& eventID = {}, const UniversallyUniqueID& uuid = {}, FlagSet<ProcessEventFlag> flags = {}) override
 			{
-				return AccessEvtHandler().DoQueueEvent(std::move(event), eventID, uuid, flags);
+				return AccessTopEvtHandler().DoQueueEvent(std::move(event), eventID, uuid, flags);
 			}
 			bool DoProcessEvent(IEvent& event, const EventID& eventID = {}, const UniversallyUniqueID& uuid = {}, FlagSet<ProcessEventFlag> flags = {}, IEvtHandler* onlyIn = nullptr) override
 			{
-				return AccessEvtHandler().DoProcessEvent(event, eventID, uuid, flags, onlyIn);
+				return AccessTopEvtHandler().DoProcessEvent(event, eventID, uuid, flags, onlyIn);
 			}
 
 			bool TryBefore(IEvent& event) override
 			{
-				return AccessEvtHandler().TryBefore(event);
+				return AccessThisEvtHandler().TryBefore(event);
 			}
 			bool TryAfter(IEvent& event) override
 			{
-				return AccessEvtHandler().TryAfter(event);
+				return AccessThisEvtHandler().TryAfter(event);
 			}
 
 		public:
 			// IEvtHandler
 			bool ProcessPendingEvents() override
 			{
-				return m_EvtHandler.ProcessPendingEvents();
+				return AsDerived().GetThisEvtHandler().ProcessPendingEvents();
 			}
 			size_t DiscardPendingEvents() override
 			{
-				return m_EvtHandler.DiscardPendingEvents();
+				return AsDerived().GetThisEvtHandler().DiscardPendingEvents();
 			}
 
 			IEvtHandler* GetPrevHandler() const override
 			{
-				return m_EvtHandler.GetPrevHandler();
+				return AsDerived().GetThisEvtHandler().GetPrevHandler();
 			}
 			IEvtHandler* GetNextHandler() const override
 			{
-				return m_EvtHandler.GetNextHandler();
+				return AsDerived().GetThisEvtHandler().GetNextHandler();
 			}
 			void SetPrevHandler(IEvtHandler* evtHandler) override
 			{
-				m_EvtHandler.SetPrevHandler(evtHandler);
+				AsDerived().GetThisEvtHandler().SetPrevHandler(evtHandler);
 			}
 			void SetNextHandler(IEvtHandler* evtHandler) override
 			{
-				m_EvtHandler.SetNextHandler(evtHandler);
+				AsDerived().GetThisEvtHandler().SetNextHandler(evtHandler);
 			}
 
 			void Unlink() override
 			{
-				m_EvtHandler.Unlink();
+				AsDerived().GetThisEvtHandler().Unlink();
 			}
 			bool IsUnlinked() const override
 			{
-				return m_EvtHandler.IsUnlinked();
+				return AsDerived().GetThisEvtHandler().IsUnlinked();
 			}
 
 			bool IsEventProcessingEnabled() const override
 			{
-				return m_EvtHandler.IsEventProcessingEnabled();
+				return AsDerived().GetThisEvtHandler().IsEventProcessingEnabled();
 			}
 			void EnableEventProcessing(bool enable = true) override
 			{
-				m_EvtHandler.EnableEventProcessing(enable);
+				AsDerived().GetThisEvtHandler().EnableEventProcessing(enable);
 			}
 	};
 }
