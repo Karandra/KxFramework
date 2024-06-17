3e0d750d781031293b8306da7dbc9fc69e4ac78c
Karandra <32564546+Karandra@users.noreply.github.com>
2024-06-17 07:53:37 +0300

    Added 'DefaultMemoryAllocator' static object and 'StdDefaultMemoryAllocator' class using it

--- kxf/Core/StandardAllocator.h
+++ kxf/Core/StandardAllocator.h
@@ -37,3 +37,60 @@ namespace kxf
 {
 	KX_API std::shared_ptr<IMemoryAllocator> GetStandardAllocator(StandardAllocatorKind kind, const StandardAllocatorConfig* config = nullptr);
 }
+
+namespace kxf
+{
+	extern IMemoryAllocator& DefaultMemoryAllocator;
+
+	template<class T>
+	class StdDefaultMemoryAllocator final
+	{
+		public:
+			using value_type = T;
+			using size_type = size_t;
+			using difference_type = ptrdiff_t;
+
+		public:
+			StdDefaultMemoryAllocator() noexcept = default;
+
+			template<class Tx>
+			StdDefaultMemoryAllocator(const StdDefaultMemoryAllocator<Tx>& other) noexcept
+			{
+			}
+
+		public:
+			IMemoryAllocator& get_allocator() const noexcept
+			{
+				return DefaultMemoryAllocator;
+			}
+			FlagSet<MemoryAllocatorFlag> get_allocation_flags() const noexcept
+			{
+				return {};
+			}
+
+			T* allocate(size_t count)
+			{
+				auto ptr = get_allocator().Allocate(count * sizeof(T), 0, get_allocation_flags());
+				if (!ptr)
+				{
+					throw std::bad_alloc();
+				}
+
+				return static_cast<T*>(ptr);
+			}
+			void deallocate(T* ptr, size_t count)
+			{
+				if (!get_allocator().Free(ptr))
+				{
+					throw std::bad_alloc();
+				}
+			}
+
+		public:
+			template<class Tx>
+			StdDefaultMemoryAllocator& operator=(const StdDefaultMemoryAllocator<Tx>& other) noexcept
+			{
+				return *this;
+			}
+	};
+}
